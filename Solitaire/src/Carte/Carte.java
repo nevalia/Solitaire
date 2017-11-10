5af932293f4b9bbd23edc092f79a0ec967be80c4
Maha Amar <amarmaha2707@gmail.com>
2017-11-10 15:29:26 +0100

    Carte début

--- Solitaire/src/Carte/Carte.java
+++ Solitaire/src/Carte/Carte.java
@@ -3,6 +3,40 @@ package Carte;
 
 public class Carte 
 {
+	private String symbole;
+	private String valeur;
+	public String couleur;
+	private boolean visible; 
+	
+	
+	public Carte(String symbole, String valeur, boolean visible) {
+		this.symbole = symbole;
+		this.valeur = valeur;
+		this.visible = visible;
+		this.setCouleur();
+	}
+
+	public String getSymbole() {
+		return symbole;
+	}
+
+	public void setSymbole(String symbole)
+	{
+		this.symbole = symbole;
+	}
+
+	public String getValeur() {
+		return valeur;
+	}
+
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+
+	public String getCouleur() {
+		return couleur;
+	}
+
 	
 	
 }
