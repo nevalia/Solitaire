5a2af45d1a8de7f5fd83eb20fb38a2174171ffaf
Maha Amar <amarmaha2707@gmail.com>
2017-11-10 17:47:45 +0100

    Première partie de colonne

--- Solitaire/src/Carte/Colonne.java
+++ Solitaire/src/Carte/Colonne.java
@@ -1,7 +1,45 @@
 package Carte;
 
+import java.util.ArrayList;
+
 public class Colonne 
 {
+	private int numeroOrdre;
+	private int nbCartes;
+	private ArrayList<Carte> listeCartes;
 	
+	public Colonne(int numeroOrdre, int nbCartes)
+	{
+		this.numeroOrdre = numeroOrdre;
+		this.nbCartes = nbCartes;
+		this.listeCartes = new ArrayList<Carte>();
+	}
+	public int getNumeroOrdre()
+	{
+		return numeroOrdre;
+	}
 	
+	public void setNumeroOrdre(int numeroOrdre)
+	{
+		this.numeroOrdre = numeroOrdre;
+	}
+	
+	public int getNbcartes()
+	{
+		return nbCartes;
+	}
+	public void setNbCartes (int nbCartes)
+	{
+		this.nbCartes = nbCartes;
+	}
+	
+	public ArrayList<Carte> getListeCartes()
+	{
+		return listeCartes;
+	}
+	
+	public void setListeCartes(ArrayList<Carte> listeCartes)
+	{
+		this.listeCartes = listeCartes;
+	}
 }
