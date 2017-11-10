44f304dcc6c59eef26856d024d06784729531a66
Chloesbrs <chloe.sbrs@orange.fr>
2017-11-10 12:11:31 +0100

    Une partie de pile

--- Solitaire/src/Carte/Pile.java
+++ Solitaire/src/Carte/Pile.java
@@ -1,10 +1,24 @@
 package Carte;
 
-
+import java.util.ArrayList;
 
 public class Pile 
 {
+	private String symbole;
+	private ArrayList<Carte> cartes;
 	
-		
+	//constructeur
+	public Pile(String symbole) {
+		this.symbole = symbole;
+		this.cartes = new ArrayList<Carte>();
+	}
+
+	public String getSymbole() {
+		return symbole;
+	}
+
+	public void setSymbole(String symbole) {
+		this.symbole = symbole;
+	}
 
 }
